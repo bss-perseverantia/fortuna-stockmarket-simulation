198de99f0334bbf560389d5727301065d5d3243f
Bumblebee-3 <bumblebee4ever05@gmail.com>
2024-07-30 08:55:19 +0530

    Add files

---
+++ database.js
@@ -0,0 +1,123 @@
+const fs = require("fs");
+const filepath = "./database.json";
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
+
+
+class Database {
+    constructor() {
+        let schools = [
+            "P3",
+            "P4",
+            "P9",
+            "P8",
+            "P10",
+            "P12",
+            "P14",
+            "P16",
+            "P18",
+            "P17",
+            "P22",
+            "P23",
+            "P25",
+            "P26",
+            "P15",
+            "P13",
+            "P20",
+            "P24",
+            "P19",
+            "P27",
+            "P29",
+            "P31"
+        ].sort()
+        var database = {
+            "schooldata": [],
+            "stockprices": []
+        };
+        database.stockprices = [
+            { "name": "Giovanni Motors", "price": 2048, "sector": "Automobiles" },
+            { "name": "Bunsel Steel", "price": 890, "sector": "Steel" },
+            { "name": "AP Cement", "price": 970, "sector": "Cement" },
+            { "name": "Costaviya", "price": 1243, "sector": "Banking" },
+            { "name": "Insula Paints", "price": 2933, "sector": "Paints" },
+            { "name": "Gajanan Housing and Construction", "price": 1430, "sector": "Real Estate (Housing)" },
+            { "name": "Musafir Travels", "price": 7200, "sector": "Tourism" },
+            { "name": "Indigo Entertainment", "price": 252, "sector": "Entertainment" },
+            { "name": "Paramol Insurance", "price": 1500, "sector": "Insurance" },
+            { "name": "Hilfi Oils", "price": 347, "sector": "Oils" },
+            { "name": "Forge X", "price": 3644, "sector": "Logistics" },
+            { "name": "Sunset Grove Resort and Hotels", "price": 421, "sector": "Hospitality" },
+            { "name": "Vibgyor Aviation", "price": 4321, "sector": "Aviation" },
+            { "name": "BuzzWave", "price": 1673, "sector": "Social Networking" },
+            { "name": "Codezen", "price": 1370, "sector": "I.T." },
+            { "name": "Fostech Communication", "price": 1000, "sector": "Telecom" },
+            { "name": "Farm 2 Table", "price": 2198, "sector": "Consumer Food" },
+            { "name": "Athlife", "price": 300, "sector": "Athleisure" },
+            { "name": "Medicare", "price": 1114, "sector": "Pharma" },
+            { "name": "Imperial Silk", "price": 750, "sector": "Textiles" }
+        ];
+        //        var accounts = [];
+        for (let i = 0; i < schools.length; i++) {
+            database.schooldata.push({
+                "schoolcode": schools[i],
+                "stocks": Array(database.stockprices.length).fill(0),
+                "cash": 1000000
+            })
+            //accounts[i] = {"username":schools[i],"password":makeid(6)};
+        }
+        const fs = require("fs")
+        //fs.writeFileSync("./database.json", JSON.stringify(database));
+        //fs.writeFileSync("./accounts.json", JSON.stringify(accounts));
+
+
+    }
+    buyStock(school, stockname) {
+        const db = require('./database.json');
+        const schoolIndex = db.schooldata.findIndex(schoolData => schoolData.schoolcode === school);
+        const stockIndex = db.schooldata[schoolIndex].stocks.findIndex((stock, index) => stockname === db.stockprices[index].name);
+        if (db.schooldata[schoolIndex].cash > db.stockprices[stockIndex].price) {
+            db.schooldata[schoolIndex].stocks[stockIndex]++;
+            db.schooldata[schoolIndex].cash -= db.stockprices[stockIndex].price;
+            fs.writeFileSync("./database.json", JSON.stringify(db));
+        }
+
+    }
+
+    sellStock(school, stockname) {
+        const db = require('./database.json');
+        const schoolIndex = db.schooldata.findIndex(schoolData => schoolData.schoolcode === school);
+        const stockIndex = db.schooldata[schoolIndex].stocks.findIndex((stock, index) => stockname === db.stockprices[index].name);
+        if (db.schooldata[schoolIndex].stocks[stockIndex] > 0) {
+            db.schooldata[schoolIndex].stocks[stockIndex]--;
+            db.schooldata[schoolIndex].cash += db.stockprices[stockIndex].price;
+            fs.writeFileSync("./database.json", JSON.stringify(db));
+        }
+    }
+    getstockList() {
+        const data = require("./database.json");
+
+        return data.stockprices;
+    }
+    setStockValue(i, prices) {
+        const data = require("./database.json");
+        data.stockprices[i].price = prices;
+        fs.writeFileSync("./database.json", JSON.stringify(data));
+    }
+    setPrice(stock, price) {
+        var data = require("./database.json");
+        data.stockprices.find(i => i.name === stock).price = price;
+        fs.writeFileSync("./database.json", JSON.stringify(data));
+    }
+}
+module.exports = {
+    Database
+}
---
+++ index.js
@@ -0,0 +1,155 @@
+const { Database } = require("./database.js");
+const db = new Database();
+const express = require("express");
+const app = express()
+app.listen(3000, () => {
+    console.log("App ready!")
+});
+let bodyParser = require("body-parser");
+let crypto = require("crypto");
+let cookies = require("cookie-parser");
+let tradetime = true;
+
+app.use(express.static("images"))
+app.use(cookies())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+
+setInterval(() => {
+    if (!tradetime) return;
+    let sl = db.getstockList();
+    for (let i = 0; i < sl.length; i++) {
+        let prices = [sl[i].price];
+        //console.log(prices)
+        let change_vector = Array(prices.length).fill(0.0); // Nudges from people buying or selling
+        //console.log(db.getstockList()) // not needed tysm
+        for (let i = 0; i < prices.length; i++) {
+            prices[i] *= 1 + (Math.random() * 2 - 1 + change_vector[i]) / 100; // Random()->(0,1], Random()*2->(0, 2], Random()*2-1->(-1, 1]
+            change_vector[i] = 0.0; // Reset change vector
+        }
+        db.setStockValue(i, parseFloat(prices[prices.length - 1].toFixed(2)));
+
+    }
+}, 2000);
+
+app.get("/sellStock", async (req, res) => {
+    const stock = req.query.stock;
+    const school = req.query.school.split("_")[0];
+    const acc = require("./accounts.json");
+    if (!tradetime) return res.json({ "error": "Trade Time is Disabled!" });
+    console.log(stock)
+    console.log(school)
+    for (let i = 0; i < acc.length; i++) {
+        let un = acc[i].username;
+        let ps = acc[i].password;
+
+        if (un == school && ps == req.query.school.split("_")[1]) {
+            db.sellStock(school, stock);
+            return res.json({ "data": "Success" })
+        }
+    }
+    return res.json({ "error": "Invalid Auth" })
+
+})
+
+app.get("/buyStock", async (req, res) => {
+    const stock = req.query.stock;
+    const school = req.query.school.split("_")[0];
+    if (!tradetime) return res.json({ "error": "Trade Time is Disabled!" });
+    const acc = require("./accounts.json");
+    console.log(stock)
+    console.log(school)
+    for (let i = 0; i < acc.length; i++) {
+        let un = acc[i].username;
+        let ps = acc[i].password;
+
+        if (un == school && ps == req.query.school.split("_")[1]) {
+            db.buyStock(school, stock);
+            return res.json({ "data": "Success" })
+        }
+    }
+    return res.json({ "error": "Invalid Auth" })
+
+})
+
+app.get("/index", (req, res) => {
+    return res.sendFile(__dirname + "/html/index.html");
+})
+
+app.get("/api/stockPrices", (req, res) => {
+    let sl = db.getstockList();
+    res.json(sl);
+})
+app.get("/", (req, res) => {
+    console.log("helo")
+    return res.sendFile(__dirname + "/html/login.html");
+})
+
+app.post("/login", (req, res) => {
+    let usr = req.body.username.toUpperCase()
+    let pass = req.body.password;
+    const acc = require("./accounts.json");
+    if (!usr || !pass) {
+        return res.redirect("/?error=Invalid%20username%20or%20password");
+    }
+    for (let i = 0; i < acc.length; i++) {
+        let un = acc[i].username;
+        let ps = acc[i].password;
+        if (un == usr && ps == pass) {
+            return res.cookie("ssid", usr + "_" + pass).redirect("/index")
+        }
+    }
+    return res.redirect("/?error=Invalid%20username%20or%20password");
+
+
+})
+
+app.get("/data", (req, res) => {
+    var d = require("./database.json");
+    d.tradetime = tradetime;
+    return res.json(d);
+})
+
+app.get("/tt", (req, res) => {
+    res.json({ "tradetime": tradetime });
+})
+
+app.get("/portfolio", (req, res) => {
+    res.sendFile(__dirname + "/html/portfolio.html");
+})
+
+app.get("/admin", (req, res) => {
+    res.sendFile(__dirname + "/html/admin.html");
+})
+const u = "admin"
+const p = "youshallnotpass"
+
+app.get("/adminauth", (req, res) => {
+    if (req.query.u == u && req.query.p == p) {
+        return res.json({ "data": "true" });
+    }
+    else {
+        return res.json({ "data": "false" });
+    }
+})
+
+app.get("/setTT", (req, res) => {
+    if (req.query.u == u && req.query.p == p) {
+        tradetime = (req.query.value === "true");
+        console.log(tradetime);
+        return res.json({ "data": "success" });
+    }
+    else {
+        return res.json({ "data": "false" });
+    }
+})
+app.get("/setSP", (req, res) => {
+    if (req.query.u == u && req.query.p == p) {
+        db.setPrice(req.query.n, req.query.value);
+        return res.json({ "data": "success" });
+    }
+    else {
+        return res.json({ "data": "false" });
+    }
+})
