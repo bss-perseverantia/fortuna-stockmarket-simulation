239d4c6089179b0dfbd137df62aef25b84af48a2
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2025-05-26 19:59:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,37 +1,20 @@
 module.exports = {
-  cash:1000000,
-  "stockprices" : [
-      {name: 'Giovanni Motors', price: 1179, sector: 'Automobiles'},
-      {name: 'Bunsel Steel', price: 894, sector: 'Steel'},
-      {name: 'AP Cement', price: 821, sector: 'Cement'},
-      {name: 'Costaviya', price: 1243, sector: 'Banking'},
-      {name: 'Chhatrapati Power', price: 562, sector: 'Energy'},
-      {
-        name: 'Gajanan Housing and Construction',
-        price: 1430,
-        sector: 'Real Estate (Housing)',
-      },
-      {name: 'Musafir Travels', price: 2319, sector: 'Tourism'},
-      {
-        name: 'Indigo Entertainment',
-        price: 252,
-        sector: 'Entertainment',
-      },
-      {name: 'Paramol Insurance', price: 1561, sector: 'Insurance'},
-      {name: 'Hilfi Oils', price: 347, sector: 'Oils'},
-      {name: 'Forge X', price: 3644, sector: 'Logistics'},
-      {
-        name: 'Sunset Grove Resort and Hotels',
-        price: 421,
-        sector: 'Hospitality',
-      },
-      {name: 'Vibgyor Aviation', price: 4321, sector: 'Aviation'},
-      {name: 'BuzzWave', price: 1673, sector: 'Social Networking'},
-      {name: 'Codezen', price: 1370, sector: 'I.T.'},
-      {name: 'Fastech Communication', price: 1170, sector: 'Telecom'},
-      {name: 'Farm 2 Table', price: 1095, sector: 'Consumer Food'},
-      {name: 'Athlife', price: 321, sector: 'Athleisure'},
-      {name: 'Medicure', price: 1114, sector: 'Pharma'},
-      {name: 'Imperial Silk', price: 754, sector: 'Textiles'},
-    ]
-}
+  cash: 1000000,
+  stockprices: [
+  {
+    "name": "MG Motors",
+    "price": 1000,
+    "sector": "Automobile"
+  },
+  {
+    "name": "Infosys",
+    "price": 2540,
+    "sector": "IT"
+  },
+  {
+    "name": "Reliance Power",
+    "price": 700,
+    "sector": "Electricity"
+  }
+]
+};
